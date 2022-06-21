715d8019c7dee9ee8f3bda2cc6c7666e2ea6be99
michellejuliet <michelle.juliet02@gmail.com>
2022-06-21 13:54:29 +0000

    Update

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-function getName() {
-  let name = document.getElementById('name').value;
-  document.getElementById('paragraph').innerHTML = `Hello ${name}, welcome to the bootcamp`;
-  /*
-  if(name !== '') {
-  document.getElementById('paragraph').innerHTML = `Hello ${name}, welcome to the bootcamp`;  
-  }*/
-}
